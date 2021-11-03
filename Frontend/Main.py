dc0c98d5ea7570499806f739c9273b6a692f77af
Jonathon <Jonatan9102@gmail.com>
2021-11-03 16:02:38 -0400

    Justin wrote the gui to add items to a kitchens menu

--- Frontend/Main.py
+++ Frontend/Main.py
@@ -1,11 +1,15 @@
 from os import name
 import sys
+import requests
+sys.path.append('../')
+from DeskFoodModels.DeskFoodLib import Kitchen, Menu, Item
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QCheckBox, QDialog, QApplication, QStackedWidget, QWidget
 from urllib.request import urlopen
 import json
 
+#--------------------Login Window--------------------
 class loginScreen(QDialog):
     def __init__(self):
         super(loginScreen, self).__init__()
@@ -17,7 +21,7 @@ class loginScreen(QDialog):
         widget.addWidget(kScreen)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-
+#--------------------Select Option Window--------------------
 class kitchenMenu(QDialog):
     def __init__(self):
         super(kitchenMenu, self).__init__()
@@ -34,6 +38,7 @@ class kitchenMenu(QDialog):
         widget.addWidget(kScreen)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
+#--------------------Kitchens Add Item Window--------------------
 class kitchenAddItem(QDialog):
     def __init__(self):
         super(kitchenAddItem, self).__init__()
@@ -44,7 +49,7 @@ class kitchenAddItem(QDialog):
         self.pizzaCheck.clicked.connect(self.unclickP)
         self.burgerCheck.clicked.connect(self.unclickB)
         self.marketCheck.clicked.connect(self.unclickM)
-
+        self.finishBTN.clicked.connect(self.finish)
 
     def goBack(self):
         widget.setCurrentIndex(widget.currentIndex() - 1)
@@ -55,31 +60,58 @@ class kitchenAddItem(QDialog):
         self.pizzaCheck.setChecked(0)
         self.burgerCheck.setChecked(0)
         self.marketCheck.setChecked(0)
+        
     def unclickD(self):
         self.freshensCheck.setChecked(0)
         self.deliCheck.setChecked(1)
         self.pizzaCheck.setChecked(0)
         self.burgerCheck.setChecked(0)
         self.marketCheck.setChecked(0)
+        
     def unclickP(self):
         self.freshensCheck.setChecked(0)
         self.deliCheck.setChecked(0)
         self.pizzaCheck.setChecked(1)
         self.burgerCheck.setChecked(0)
         self.marketCheck.setChecked(0)
+        
     def unclickB(self):
         self.freshensCheck.setChecked(0)
         self.deliCheck.setChecked(0)
         self.pizzaCheck.setChecked(0)
         self.burgerCheck.setChecked(1)
         self.marketCheck.setChecked(0)
+        
     def unclickM(self):
         self.freshensCheck.setChecked(0)
         self.deliCheck.setChecked(0)
         self.pizzaCheck.setChecked(0)
         self.burgerCheck.setChecked(0)
         self.marketCheck.setChecked(1)
+        
+    def finish(self):
+        if(len(self.textName.toPlainText()) > 0):
+            if(len(self.textCost.toPlainText()) > 0):
+                if((self.freshensCheck.checkState()) or (self.deliCheck.checkState()) or (self.pizzaCheck.checkState()) or (self.burgerCheck.checkState()) or self.marketCheck.checkState()):
+                    available = False
+                    if(self.checkBox.checkState()): available = True
+                    if(self.freshensCheck.checkState()): mykitchen = "Freshens"
+                    if(self.deliCheck.checkState()): mykitchen = "Deli"
+                    if(self.pizzaCheck.checkState()): mykitchen = "Pizza"
+                    if(self.burgerCheck.checkState()): mykitchen = "Burgers"
+                    if(self.marketCheck.checkState()): mykitchen = "Market"
+                    item = Item(name = self.textName.toPlainText(), price = self.textCost.toPlainText(), available = available)
+                    r = requests.put("http://localhost:8000/AddToMenu/" + mykitchen, item.json())
+                    self.textName.setText("")
+                    self.textCost.setText("")
+                    self.checkBox.setChecked(0)
+                    self.freshensCheck.setChecked(0)
+                    self.deliCheck.setChecked(0)
+                    self.pizzaCheck.setChecked(0)
+                    self.burgerCheck.setChecked(0)
+                    self.marketCheck.setChecked(0)
 
+#--------------------Kitchens and Menu Window--------------------
 class KitchensScreen(QDialog):
     def __init__(self):
         super(KitchensScreen, self).__init__()
@@ -125,9 +157,7 @@ class KitchensScreen(QDialog):
         self.menuList.clear()
         self.menuList.addItems(list(data_json.keys()))
 
-
-
-
+#--------------------MAIN--------------------
 #Setting up App
 app = QApplication(sys.argv)
 loginScreen = loginScreen()
